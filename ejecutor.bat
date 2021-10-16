fa160a054c7ba5e22bbc26670b1d770a76bd82a3
Jesus <v3ralt@gmail.com>
2021-10-15 20:51:21 -0500

    quinto commit

--- ejecutor.bat
+++ ejecutor.bat
@@ -1,2 +1,2 @@
-cmd /k "cd virtualenv & cd Scripts & activate & cd .. & cd .. & app.py
+cmd /k "cd virtualenv & cd Scripts & activate & cd .. & cd .. &pip install -r requirements.txt & app.py
 cmd /k "cd C:\Users\3PX52LA_RS6\Documents\Proyectos WEB\Examen (Ctalogo de productos)\virtualenv\Scripts & deactivate & exit
